f728073417af8db14826c1731e687e813fec194d
branson8815 <96606008+branson8815@users.noreply.github.com>
2024-12-08 17:24:24 -0800

    added a method to insert innerHTML to each square of the grid

--- wordsearch/javascript/main.js
+++ wordsearch/javascript/main.js
@@ -4,6 +4,7 @@ import GameBoard from "./modules/GameBoard.js";
 let board = new GameBoard(10, 10)
 
 board.createBoard()
-
+board.addLetters()
 console.log('main.js loaded');
 
+console.log(board.grid)
