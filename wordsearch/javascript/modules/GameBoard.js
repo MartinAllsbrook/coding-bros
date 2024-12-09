b25fb7b94d755ceb67deb00b84fc57eee7f3f92c
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-08 19:14:33 -0800

    Cleaned Up Some Code

--- wordsearch/javascript/modules/GameBoard.js
+++ wordsearch/javascript/modules/GameBoard.js
@@ -8,46 +8,31 @@ export default class GameBoard {
     createBoard() {
         let gameboard = document.getElementById("gameboard")
         
-        
-
-
         for(let y = 0; y<this.height; y++){
-           let row = document.createElement('div');
-           row.className = "row"
-           row.id = "row" + y;
-           gameboard.appendChild(row);
-           console.log("added row");
-           this.grid[y] = [];
-           for(let x = 0; x<this.width; x++){
-            let column = document.createElement('div');
-            column.className ="column"
-            column.id = "row_" + y+ "_column" +x;
-            row.appendChild(column);
-            console.log("added column");
-            this.grid[y][x] = column
-             
- 
- 
- 
-         }
-
-
-
+            let row = document.createElement('div');
+            row.className = "row"
+            row.id = "row" + y;
+            gameboard.appendChild(row);
+            console.log("added row");
+            this.grid[y] = [];
+            
+            for(let x = 0; x<this.width; x++){
+                let column = document.createElement('div');
+                column.className ="column"
+                column.id = "row_" + y+ "_column" +x;
+                row.appendChild(column);
+                console.log("added column");
+                this.grid[y][x] = column
+            }
         }
-        
     }
-    
 
     addLetters() {
-
         for(let row = 0; row < this.height; row++) {
             for (let column = 0; column<this.width; column++){
                 this.grid[column][row].innerHTML = 'A';
                 console.log("Added letter");
-
             }
-
         }
     }
-
 }
---
+++ wordsearch/javascript/modules/Word.js
@@ -0,0 +1,8 @@
+export default class Word {
+    constructor(word, x, y, direction) {
+        this.word = word;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }   
+}
