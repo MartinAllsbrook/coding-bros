87f1494ec2a1727ce45acf0e8bfd56cd2bbc1bcf
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-08 16:26:15 -0800

    Update GameBoard.js

---
+++ wordsearch/javascript/modles/GameBoard.js
@@ -0,0 +1,11 @@
+export default class GameBoard {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;        
+    }
+
+    createBoard() {
+        
+    }
+    
+}
