0080a08c3d50f80ac8ca4badd997265b29475368
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-09 19:34:45 -0800

    Update Dicstionary.js
    Added getRandomCharacter() Method to dictionary

--- wordsearch/javascript/modules/Dicstionary.js
+++ wordsearch/javascript/modules/Dicstionary.js
@@ -18,10 +18,17 @@ export default class Dictionary {
             'WIFI',
             'ETHERNET'
         ];
+
+        this.characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     }
 
     getRandomWord() {
         const randomIndex = Math.floor(Math.random() * this.words.length);
         return this.words[randomIndex];
+    }
+
+    getRandomCharacter(characters) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        return characters.charAt(randomIndex);
     }
 }
