59da946f77234f4c49f8208629bcb3529c3e8800
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-09 20:55:19 -0800

    Misc Dictionary Updates+
    Added comments to dictionary, used dictionary in the main.js testing commands
    
    Renamed dictionary.js

--- wordsearch/javascript/main.js
+++ wordsearch/javascript/main.js
@@ -1,17 +1,15 @@
 import GameBoard from "./modules/GameBoard.js";
 import Word from "./modules/Word.js";
+import Dictionary from "./modules/Dictionary.js";
 
 let board = new GameBoard(13, 15)
+let dictionary = new Dictionary()
 
 board.createBoard()
 
-let test_word = new Word("Coder", 4, 4, "diagonal");
-
-let test_word_2 = new Word("Bro", 9, 9, "vertical");
+let test_word = new Word(dictionary.getRandomWord(), 8, 2, "diagonal");
+let test_word_2 = new Word(dictionary.getRandomWord(), 11, 4, "vertical");
 
 console.log(test_word);
 board.addLetters(test_word);
 board.addLetters(test_word_2);
-// console.log('main.js loaded');
-
-// console.log(board.grid)
