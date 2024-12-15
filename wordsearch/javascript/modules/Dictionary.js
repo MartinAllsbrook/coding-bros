489aafddaff69db1edf01be3fb9894004cd657c4
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-15 13:32:33 -0800

    Update Dictionary.js

--- wordsearch/javascript/modules/Dictionary.js
+++ wordsearch/javascript/modules/Dictionary.js
@@ -1,36 +1,35 @@
 export default class Dictionary {
-        constructor() {
-            this.words = [
-                'JAVASCRIPT',
-                'HTML',
-                'CSS',
-                'CODE',
-                'WEBSITE',
-                'DEVELOPER',
-                'PROGRAMMER',
-                'COMPUTER',
-                'MOUSE',
-                'KEYBOARD',
-                'MONITOR',
-                'SCREEN',
-                'INTERNET',
-                'NETWORK',
-                'WIFI',
-                'ETHERNET'
-            ];
-            
-        }
-        // Returns a random word from the words array
-        getRandomWord() {
-            const randomIndex = Math.floor(Math.random() * this.words.length);
-            return this.words[randomIndex];
-        }
-
-        // Returns a random character from the characters string
-        getRandomCharacter() {
-            
-            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            return characters.charAt(randomIndex);
-        }
+    constructor() {
+        this.words = [
+            'JAVASCRIPT',
+            'HTML',
+            'CSS',
+            'CODE',
+            'WEBSITE',
+            'DEVELOPER',
+            'PROGRAMMER',
+            'COMPUTER',
+            'MOUSE',
+            'KEYBOARD',
+            'MONITOR',
+            'SCREEN',
+            'INTERNET',
+            'NETWORK',
+            'WIFI',
+            'ETHERNET'
+        ];
+        
+        this.characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     }
+    // Returns a random word from the words array
+    getRandomWord() {
+        const randomIndex = Math.floor(Math.random() * this.words.length);
+        return this.words[randomIndex];
+    }
+
+    // Returns a random character from the characters string
+    getRandomCharacter() {
+        const randomIndex = Math.floor(Math.random() * this.characters.length);
+        return this.characters.charAt(randomIndex);
+    }
+}
