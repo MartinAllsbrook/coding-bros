e86eec53a3eeb2c65da554c0be8b34226d7462f7
clairebarr <clairebarr@icloud.com>
2024-12-21 17:38:29 -0500

    styling folder title etc

--- desktop/main.js
+++ desktop/main.js
@@ -9,10 +9,6 @@ document.addEventListener('mousemove', (event) => {
         console.log('Mouse position:', x, y);
 });
 
-const window = document.getElementsByClassName('window')[0];
-console.log(window);
-window.style.backgroundColor = "lightgray";
-window.style.borderRadius = "10px";
 
 testInnerWidth();
 
@@ -31,45 +27,21 @@ function testInnerWidth() {
     console.log(top.innerWidth);
 }
 
-// Function to change color of window on click
-const changeColor = document.getElementsByClassName('changing-color')[0];
-const unchangeColor = document.getElementsByClassName('unchange-color')[0];
-const windowMessage = document.getElementById('window-message');
-
-// Add an event listener to the button that listens for the click event
-changeColor.addEventListener('click', function() { 
-    // This function will be executed when the button is clicked
-    console.log("Button clicked!"); 
-    window.style.backgroundColor = "lightblue";
-    window.style.borderRadius = "50%";
-    windowMessage.innerHTML = "Hi Martin :) You clicked the button!";
-  });
-
-unchangeColor.addEventListener('click', function() { 
-    window.style.backgroundColor = "lightgray";
-    windowMessage.innerHTML = "Hello World";
-    window.style.borderRadius = "10px";
-});
-
 
 const openWindow = document.getElementsByClassName('open-folder')[0];
 const secondWindow = document.getElementsByClassName('window-two')[0];
-const a = 1;
-const b = 2;
-let myLuchadora = a > b;
+const folderBackground = document.getElementsByClassName('folder-bg')[0];
 let trueButton = false;
-
-console.log(trueButton);
 
 openWindow.addEventListener('click', function() {
 
     if (trueButton === true) {
         secondWindow.style.display = "none";
-        openWindow.className = 'open-folder close';
+        folderBackground.className = 'folder-bg close';
         trueButton = false;
     } else { 
         secondWindow.style.display = "block";
-        openWindow.className = 'open-folder open';
+        folderBackground.className = 'folder-bg open';
         trueButton = true;
     }
 });
