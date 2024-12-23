2e5d7092c773ade2d45aa9cc97407ce734213e9f
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-22 18:35:57 -0800

    Added cube that looks at cursor!

--- threeJSExperiments/main.js
+++ threeJSExperiments/main.js
@@ -3,11 +3,12 @@ import * as THREE from 'three';
 // Materials
 let mp_green = {
     color: 0x00ff00,
-    blendAlpha: 0.5
 };
 let mp_blue = {
     color: 0x0000ff,
-    blendAlpha: 0.5
+};
+let mp_red = {
+    color: 0xff0000,
 };
 
 // Create scene 
@@ -27,22 +28,27 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
 // Green Matcap Material cube
-const material = new THREE.MeshMatcapMaterial( mp_green );
+const material = new THREE.MeshPhongMaterial( mp_red );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.x = 1;
+cube.position.x = 2;
 scene.add( cube );
 
 const material2 = new THREE.MeshPhysicalMaterial( mp_blue );
 const cube2 = new THREE.Mesh( geometry, material2 );
+cube2.position.x = -2;
 scene.add( cube2 );
 
+const material3 = new THREE.MeshMatcapMaterial( mp_green );
+const cube3 = new THREE.Mesh( geometry, material3 );
+scene.add( cube3 );
 // Create a directional light
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add( directionalLight );
 
+// ### Functions ###
+
 // Basic animation function for the cube 
 function animate() {
-
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
@@ -50,5 +56,31 @@ function animate() {
 	cube2.rotation.y -= 0.01;
 
 	renderer.render( scene, camera );
-
 }
+
+function lookAtScreenPos(object, x, y){
+    const windowHalfX = window.innerWidth / 2;
+    const windowHalfY = window.innerHeight / 2;
+    x = -(x - windowHalfX) / (windowHalfX / 5);
+    y = (y - windowHalfY) / (windowHalfY / 5);
+
+    object.lookAt(x, y, -10);
+}
+
+// ### Event Listeners ###
+
+// Event listener for window resizing
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+});
+
+// Event listener for mouse movement
+document.addEventListener('mousemove', (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+    // console.log(x, y);
+
+    lookAtScreenPos(cube3, x, y);
+});
