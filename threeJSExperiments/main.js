0c838486a26c1cfee1b7337ec4b499a3c9fd6d78
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-12-22 18:24:36 -0800

    Played around with 3js
    Added a seccond cube
    learned materials a little bit
    Ya DIIIIIG

--- threeJSExperiments/main.js
+++ threeJSExperiments/main.js
@@ -1,53 +1,54 @@
 import * as THREE from 'three';
 
+// Materials
+let mp_green = {
+    color: 0x00ff00,
+    blendAlpha: 0.5
+};
+let mp_blue = {
+    color: 0x0000ff,
+    blendAlpha: 0.5
+};
+
+// Create scene 
 const scene = new THREE.Scene();
+
+// Create a camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 camera.position.z = 5;
 
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate ); 
+document.body.appendChild( renderer.domElement );
+
+// Basic Box Geometry
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+// Green Matcap Material cube
+const material = new THREE.MeshMatcapMaterial( mp_green );
+const cube = new THREE.Mesh( geometry, material );
+cube.position.x = 1;
+scene.add( cube );
+
+const material2 = new THREE.MeshPhysicalMaterial( mp_blue );
+const cube2 = new THREE.Mesh( geometry, material2 );
+scene.add( cube2 );
+
+// Create a directional light
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+
+// Basic animation function for the cube 
 function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
+    cube2.rotation.x -= 0.01;
+	cube2.rotation.y -= 0.01;
+
 	renderer.render( scene, camera );
 
 }
-
-/* import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/OrbitControls.js';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-const animate = function () {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-};
-
-animate(); */
